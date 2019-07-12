255c2da130835ebd6df75a128a7262ec44ea9227
Wqxleo <wangquanxiu818@163.com>
2019-07-12 20:00:22 +0800

    人脸识别

---
+++ create_dataset.py
@@ -0,0 +1,109 @@
+# -*-coding: utf-8 -*-
+"""
+    @Project: faceRecognition
+    @File   : create_dataset.py
+    @Author : panjq
+    @E-mail : pan_jinquan@163.com
+    @Date   : 2018-12-07 11:31:09
+"""
+import numpy as np
+from utils import image_processing , file_processing,debug
+import face_recognition
+import cv2
+import os
+
+resize_width = 160
+resize_height = 160
+
+
+def get_face_embedding(model_path,files_list, names_list):
+    '''
+    获得embedding数据
+    :param files_list: 图像列表
+    :param names_list: 与files_list一一的名称列表
+    :return:
+    '''
+    # 转换颜色空间RGB or BGR
+    colorSpace="RGB"
+    # 初始化mtcnn人脸检测
+    face_detect = face_recognition.Facedetection()
+    # 初始化facenet
+    face_net = face_recognition.facenetEmbedding(model_path)
+
+    embeddings=[] # 用于保存人脸特征数据库
+    label_list=[] # 保存人脸label的名称，与embeddings一一对应
+    for image_path, name in zip(files_list, names_list):
+        print("processing image :{}".format(image_path))
+        image = image_processing.read_image_gbk(image_path, colorSpace=colorSpace)
+        # 进行人脸检测，获得bounding_box
+        bboxes, landmarks = face_detect.detect_face(image)
+        bboxes, landmarks =face_detect.get_square_bboxes(bboxes, landmarks,fixed="height")
+        # image_processing.show_image_boxes("image",image,bboxes)
+        if bboxes == [] or landmarks == []:
+            print("-----no face")
+            continue
+        if len(bboxes) >= 2 or len(landmarks) >= 2:
+            print("-----image have {} faces".format(len(bboxes)))
+            continue
+        # 获得人脸区域
+        face_images = image_processing.get_bboxes_image(image, bboxes, resize_height, resize_width)
+        # 人脸预处理，归一化
+        face_images = image_processing.get_prewhiten_images(face_images,normalization=True)
+        # 获得人脸特征
+        pred_emb = face_net.get_embedding(face_images)
+        embeddings.append(pred_emb)
+        # 可以选择保存image_list或者names_list作为人脸的标签
+        # 测试时建议保存image_list，这样方便知道被检测人脸与哪一张图片相似
+        # label_list.append(image_path)
+        label_list.append(name)
+    return embeddings,label_list
+
+def create_face_embedding(model_path,dataset_path,out_emb_path,out_filename):
+    '''
+
+    :param model_path: faceNet模型路径
+    :param dataset_path: 人脸数据库路径，每一类单独一个文件夹
+    :param out_emb_path: 输出embeddings的路径
+    :param out_filename: 输出与embeddings一一对应的标签
+    :return: None
+    '''
+    files_list,names_list=file_processing.gen_files_labels(dataset_path,postfix=['*.jpg'])
+    embeddings,label_list=get_face_embedding(model_path,files_list, names_list)
+    print("label_list:{}".format(label_list))
+    print("have {} label".format(len(label_list)))
+
+    embeddings=np.asarray(embeddings)
+    np.save(out_emb_path, embeddings)
+    file_processing.write_list_data(out_filename, label_list, mode='w')
+
+def create_face_embedding_for_bzl(model_path,dataset_path,out_emb_path,out_filename):
+    '''
+    :param model_path: faceNet模型路径
+    :param dataset_path: 人脸数据库路径，图片命名方式：张三_XXX_XXX.jpg,其中“张三”即为label
+    :param out_emb_path: 输出embeddings的路径
+    :param out_filename: 输出与embeddings一一对应的标签
+    :return: None
+    '''
+    image_list = file_processing.get_images_list(dataset_path, postfix=['*.jpg', '*.png'])
+    names_list=[]
+    for image_path in image_list:
+        basename = os.path.basename(image_path)
+        names = basename.split('_')[0]
+        names_list.append(names)
+    embeddings,label_list=get_face_embedding(model_path,image_list, names_list)
+    print("label_list:{}".format(label_list))
+    print("have {} label".format(len(label_list)))
+    embeddings=np.asarray(embeddings)
+    np.save(out_emb_path, embeddings)
+    file_processing.write_data(out_filename, label_list, mode='w')
+
+if __name__ == '__main__':
+    model_path = 'models/20180408-102900'
+    dataset_path='dataset/images'
+    out_emb_path = 'dataset/emb/faceEmbedding.npy'
+    out_filename = 'dataset/emb/name.txt'
+    create_face_embedding(model_path, dataset_path,out_emb_path, out_filename)
+    # dataset_path = 'E:/Face/dataset/bzl/test2/facebank'
+    # dataset_path = 'E:/Face/dataset/bzl/test3/facebank'
+    # dataset_path='E:/Face/dataset/bzl/subjectphoto_with_name' #人脸数据库的路径
+    # create_face_embedding_for_bzl(model_path, dataset_path,out_emb_path, out_filename)
---
+++ predict.py
@@ -0,0 +1,81 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+import cv2
+from scipy import misc
+import tensorflow as tf
+import numpy as np
+import os
+from utils import file_processing,image_processing
+import face_recognition
+resize_width = 160
+resize_height = 160
+
+def face_recognition_image(model_path,dataset_path, filename,image_path):
+    # 加载数据库的数据
+    dataset_emb,names_list=load_dataset(dataset_path, filename)
+    # 初始化mtcnn人脸检测
+    face_detect=face_recognition.Facedetection()
+    # 初始化facenet
+    face_net=face_recognition.facenetEmbedding(model_path)
+
+    image = image_processing.read_image_gbk(image_path)
+    # 获取 判断标识 bounding_box crop_image
+    bboxes, landmarks = face_detect.detect_face(image)
+    bboxes, landmarks = face_detect.get_square_bboxes(bboxes, landmarks, fixed="height")
+    if bboxes == [] or landmarks == []:
+        print("-----no face")
+        exit(0)
+    print("-----image have {} faces".format(len(bboxes)))
+    face_images = image_processing.get_bboxes_image(image, bboxes, resize_height, resize_width)
+    face_images = image_processing.get_prewhiten_images(face_images)
+    pred_emb=face_net.get_embedding(face_images)
+    pred_name,pred_score=compare_embadding(pred_emb, dataset_emb, names_list)
+    # 在图像上绘制人脸边框和识别的结果
+    show_info=[ n+':'+str(s)[:5] for n,s in zip(pred_name,pred_score)]
+    image_processing.show_image_bboxes_text("face_recognition", image,bboxes,show_info)
+
+
+def load_dataset(dataset_path,filename):
+    '''
+    加载人脸数据库
+    :param dataset_path: embedding.npy文件（faceEmbedding.npy）
+    :param filename: labels文件路径路径（name.txt）
+    :return:
+    '''
+    embeddings=np.load(dataset_path)
+    names_list=file_processing.read_data(filename,split=None,convertNum=False)
+    return embeddings,names_list
+
+def compare_embadding(pred_emb, dataset_emb, names_list,threshold=0.65):
+    '''
+        将待识别的人脸特征向量与人脸数据库进行比较
+        :param pred_emb: 待识别的人脸
+        :param dataset_emb: 人脸数据库特征向量
+        :param threshold： 置信阈值，小于说明是同一个人，大于说明不是同一个人
+        :return:预测人名，预测分值
+        '''
+    # 为bounding_box 匹配标签
+    pred_num = len(pred_emb)
+    dataset_num = len(dataset_emb)
+    pred_name = []
+    pred_score=[]
+    for i in range(pred_num):
+        dist_list = []
+        for j in range(dataset_num):
+            dist = np.sqrt(np.sum(np.square(np.subtract(pred_emb[i, :], dataset_emb[j, :]))))
+            dist_list.append(dist)
+        min_value = min(dist_list)
+        pred_score.append(min_value)
+        if (min_value > threshold):
+            pred_name.append('unknow')
+        else:
+            pred_name.append(names_list[dist_list.index(min_value)])
+    return pred_name,pred_score
+
+if __name__=='__main__':
+    model_path='models/20180408-102900'
+    dataset_path='dataset/emb/faceEmbedding.npy'
+    filename='dataset/emb/name.txt'
+    image_path='dataset/test_images/1.jpg'
+    face_recognition_image(model_path, dataset_path, filename,image_path)
